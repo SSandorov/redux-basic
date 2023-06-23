424e13be46f22aa393c8722150124e46c0af55f7
Stefan <stefansandorovssb@gmail.com>
2023-06-23 11:54:42 +0200

    uso básico de una acción y un reducer

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log('Hello World');
+// * Acciones
+
+// * El payload es un parámetro que podemos pasarle a la acción. Un ejemplo de 
+// * esto es cuando queremos pasarle un id a la acción para que sepa qué elemento modificar.
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+// * El reducer es una función que recibe un estado y una acción y devuelve un nuevo estado.
+function reducer(state = 10, action: Action) {
+
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+
+    // ! El reducer siempre tiene que regresar un estado
+    return state;
+}
+
+// Usar el reducer
+
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+}
+
+console.log(reducer(10, incrementadorAction));
